eba5d1c00a4096bcc53c9da5c4b434e7e2210d6c
Bob Ellis <bob.ellis@modop.com>
2016-04-18 15:49:16 -0700

    working on styling

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,13 +1,31 @@
 $(document).ready(function(){
+    $(".menu-button").click(function(event){
+        if(!($("dropdown-content").is(":visible"))){
+            $(".dropdown-content").show("fade", "fast");
+        } else {
+            $(".dropdown-content").hide("fade", "fast");
+        }
+    })
+
+    // $("body").click(function(event){
+    //     if($("dropdown-content").is(":visible")){
+    //         $(".dropdown").("fade", "fast");
+    //     }
+    // })
+
     $('#about-me-link').click(function(event){
         $('#projects').hide("fade", "fast");
         $('#about-me').show("fade", "fast");
+        $(".dropdown-content").hide("fade", "fast");
     });
 
     $('#projects-link').click(function(event){
         $('#projects').show("fade", "fast");
         $('#about-me').hide("fade", "fast");
+        $(".dropdown-content").hide("fade", "fast");
     });
 
+    $("body").click(function(event){
 
+    })
 })
